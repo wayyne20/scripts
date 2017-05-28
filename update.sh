ac8bb363057a8c63a4953474cadf22bf67e83f72
wayyne20 <wayyne20@gmail.com>
2017-05-28 21:03:34 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,17 @@
-#! /bin/bash
+#!/bin/bash
+#
+# Update script by w10a6
+#
+# 
+# 	
+#	
+#
 clear
+echo "---------------------------------"
+echo "updateing now...."
+echo "---------------------------------"
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+sudo apt-get upgrade -yy
+echo "---------------------------------"
+echo "Done"
+echo "---------------------------------"
